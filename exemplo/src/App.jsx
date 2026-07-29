176b8f57a0f6371691c9f7be1f6a0f551d8e7c24
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-07-28 21:27:01 -0300

    aula 5 renderizacao condicional

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,9 +1,21 @@
-import ListaDeProdutos from "./components/aula4/ListaDeProdutos";
+import { useState } from "react";
+import LoginStatus from "./LoginStatus";
+import Notification from "./Notification";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showNotif, setShowNotif] = useState(false);
+
   return (
     <div>
-      <ListaDeProdutos />
+      <LoginStatus isLoggedIn={isLoggedIn} />
+      <Notification showNotification={showNotif} />
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
+      <button onClick={() => setShowNotif(!showNotif)}>
+        {showNotif ? "Ocultar" : "Mostrar"} Notificação
+      </button>
     </div>
   );
 }
