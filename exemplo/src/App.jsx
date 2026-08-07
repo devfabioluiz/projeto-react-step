398967c85ece1f75e7f58450b8a0fbfffde0e932
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-08-06 21:43:40 -0300

    aula8 react fix

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,13 +1,11 @@
-import ButtonTailwind from "./components/aula7/ButtonTailwind";
+import Usuarios from "./components/aula8/Usuarios";
 
 function App() {
   return (
     <div>
-      <h1>Botão com Tailwind CSS</h1>
-      <ButtonTailwind label="Primário" primary />
-      <ButtonTailwind label="Secundário" />
+      <h1>JSONPlaceholder App</h1>
+      <Usuarios />
     </div>
   );
 }
-
 export default App;
