1041d1086b517dd8353dbaf513c0a64c8c9d66cd
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-08-08 08:37:28 -0300

    aula 8 requisicoes http

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,10 +1,14 @@
 import Usuarios from "./components/aula8/Usuarios";
+import Posts from "./components/aula8/Posts";
+import ListaUsuarios from "./components/aula8/ListaUsuarios";
 
 function App() {
   return (
     <div>
       <h1>JSONPlaceholder App</h1>
       <Usuarios />
+      <Posts />
+      <ListaUsuarios />
     </div>
   );
 }
