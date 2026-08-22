6dd83271947df1e80cce01f9af807856de18d733
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-08-22 10:08:17 -0300

    aula 4

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,9 +1,11 @@
 import ListaDeProdutos from "./components/aula4/ListaDeProdutos";
+import Exemplo from "./components/aula4/Exemplo";
 
 function App() {
   return (
     <div>
-      <ListaDeProdutos />
+      {/* <ListaDeProdutos /> */}
+      <Exemplo />
     </div>
   );
 }
---
+++ exemplo/src/components/aula4/Exemplo.jsx
@@ -0,0 +1,15 @@
+const Exemplo = () => {
+  let lista = [1, 2, 3];
+
+  return (
+    <ul>
+      {lista
+        .filter((valor) => valor % 2 === 0)
+        .map((valor) => (
+          <li key={valor}>{valor}</li>
+        ))}
+    </ul>
+  );
+};
+
+export default Exemplo;
