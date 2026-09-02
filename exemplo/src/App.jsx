d387fb6fa71966d55f1e6f9730d801ab844f9783
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-09-01 21:31:47 -0300

    aula 12

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,37 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/aula10/Home";
-import Login from "./components/aula10/Login";
-import About from "./components/aula10/About";
-import Contact from "./components/aula10/Contact";
-import User from "./components/aula10/User";
-import PrivateRoute from "./components/aula10/PrivateRoute";
-import Dashboard from "./components/aula10/Dashboard";
-import Profile from "./components/aula10/Profile";
-import Settings from "./components/aula10/Settings";
+import { ThemeProvider } from "./ThemeContext";
+import ThemeSwitcher from "./ThemeSwitcher";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <ThemeSwitcher />
+      // outros componentes aqui...
+    </ThemeProvider>
   );
 };
 
